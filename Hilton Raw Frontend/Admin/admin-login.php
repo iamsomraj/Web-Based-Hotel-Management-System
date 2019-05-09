32ce10b2cc4899c183649f557b4acaa31a01d717
Somraj Mukherjee <iamsomraj@gmail.com>
2019-05-09 13:55:23 +0530

    File extension is changed in admin folder of secure

---
+++ Hilton Raw Frontend/Admin/admin-login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html style="background-image:url(&quot;../assets/img/background-sm.jpg&quot;);">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="../assets/css/DashBoard-light-boostrap1.css">
+    <link rel="stylesheet" href="../assets/css/DashBoard-light-boostrap2.css">
+    <link rel="stylesheet" href="../assets/css/DashBoard-light-boostrap4.css">
+    <link rel="stylesheet" href="../assets/css/DashBoard-light-boostrap3.css">
+    <link rel="stylesheet" href="../assets/css/Carousel-Hero.css">
+    <link rel="stylesheet" href="../assets/css/DashBoard-light-boostrap.css">
+    <link rel="stylesheet" href="../assets/css/Features-Blue.css">
+    <link rel="stylesheet" href="../assets/css/Features-Boxed.css">
+    <link rel="stylesheet" href="../assets/css/Features-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="../assets/css/Header-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <link rel="stylesheet" href="../assets/css/Google-Style-Login.css">
+    <link rel="stylesheet" href="../assets/css/Lightbox-Gallery.css">
+    <link rel="stylesheet" href="../assets/css/Pretty-Header.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Responsive-Form.css">
+    <link rel="stylesheet" href="../assets/css/Responsive-Form1.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu1.css">
+    <link rel="stylesheet" href="../assets/css/sidebar.css">
+    <link rel="stylesheet" href="../assets/css/sidebar1.css">
+    <link rel="stylesheet" href="../assets/css/sticky-dark-top-nav-with-dropdown.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/Team-Boxed.css">
+    <link rel="stylesheet" href="../assets/css/Testimonials.css">
+</head>
+
+<body style="background-color:rgba(250,250,250,0);">
+    <div class="container">
+        <div class="login-card"><img src="../assets/img/avatar_2x.png" class="profile-img-card">
+            <p class="profile-name-card"> </p>
+            <form class="form-signin"><span class="reauth-email"> </span><input class="form-control" type="email" name="username" required="" placeholder="Username" autofocus="" id="inputEmail"><input class="form-control" type="password" required="" placeholder="Password" id="inputPassword">
+                <div
+                    class="checkbox">
+                    <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"></div>
+        </div><button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button></form>
+    </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/DashBoard-light-boostrap2.js"></script>
+    <script src="../assets/js/DashBoard-light-boostrap.js"></script>
+    <script src="../assets/js/DashBoard-light-boostrap1.js"></script>
+    <script src="../assets/js/DashBoard-light-boostrap4.js"></script>
+    <script src="../assets/js/DashBoard-light-boostrap3.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+    <script src="../assets/js/Sidebar-Menu.js"></script>
+</body>
+
+</html>
