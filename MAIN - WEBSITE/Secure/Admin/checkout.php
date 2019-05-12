dcc75153833cebe9c350811cf9c10cc7fb05e9cd
jagriti47 <50318934+jagriti47@users.noreply.github.com>
2019-05-12 22:50:13 +0530

    rooms updated after checkout

--- MAIN - WEBSITE/Secure/Admin/checkout.php
+++ MAIN - WEBSITE/Secure/Admin/checkout.php
@@ -1,12 +1,17 @@
 <?php
+  include("connection.php");
 $r="";
-if(isset($_GET["id"]))
+if(isset($_GET["id"])  && isset($_GET["roomtype"]))
 {
-    if($_GET["id"] > 0)
+    if($_GET["id"] > 0  &&  $_GET["roomtype"] != "")
     {
-        include("connection.php");
+      
 
         $id = $_GET["id"];
+        $roomtype = $_GET["roomtype"];
+          
+        
+        
         $sql="SELECT * FROM `confirmbooking` WHERE `confirmbooking`.`id` = \"$id\" " ;
         $resultone=mysqli_query($link,$sql);
         $name="";
@@ -20,6 +25,9 @@ if(isset($_GET["id"]))
         $payment="";
         $paymethod="";
         $customerid="";
+        
+
+        
             
         while($row=mysqli_fetch_array($resultone))
         {
@@ -40,26 +48,33 @@ if(isset($_GET["id"]))
 
 
                 break;
-        
-            
+
         }
         
   
         
   
         $query = "INSERT INTO `completebooking` (`id`, `name`, `email`, `contact`, `address`, `roomtype`, `checkin`, `checkout`, `noofdays`, `payment`, `paymethod`, `customerid`) VALUES (NULL, \"$name\", \"$email\",\"$contact\", \"$address\", \"$roomtype\", \"$checkin\", \"$checkout\", \"$noofdays\", \"$payment\", \"$paymethod\", \"$id\")";
+        
+        
+        
 
-        $resulttwo = mysqli_query($link,$query);
+       $resulttwo = mysqli_query($link,$query);
     
         $query = "DELETE FROM `confirmbooking` WHERE `confirmbooking`.`id` = ".$id;
         $result = mysqli_query($link,$query);                                         
 
+        //header("location:admin-history.php");
+        
+        
+        //updating vacant rooms
+        
+        $sql="UPDATE `room` SET `vacant`=`vacant`+1 where `roomtype`=\"$roomtype\" ";
+        $result1=mysqli_query($link,$sql);
         header("location:admin-history.php");
 
         
-
-        
-                                                                       
+                                                                                                     
                 
     }
     else
