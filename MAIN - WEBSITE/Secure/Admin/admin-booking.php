2005146c37e606d96f63560790f6c3cf3d4d4475
Somraj Mukherjee <iamsomraj@gmail.com>
2019-05-12 13:56:24 +0530

    updated admin booking

--- MAIN - WEBSITE/Secure/Admin/admin-booking.php
+++ MAIN - WEBSITE/Secure/Admin/admin-booking.php
@@ -6,7 +6,6 @@
 
 <html>
 
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,9 +17,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-
-    <link rel="stylesheet" href="../assets/css/Carousel-Hero.css">
-
     <link rel="stylesheet" href="../assets/css/Features-Blue.css">
     <link rel="stylesheet" href="../assets/css/Features-Boxed.css">
     <link rel="stylesheet" href="../assets/css/Features-Clean.css">
@@ -130,7 +126,7 @@
                 <td><?php echo $paymethod; ?></td>
                     
                 <th>
-                <div class="btn-group btn-outline-dark">
+                <div class="btn-group-vertical">
                     <button type="button" class='btn btn-success bg-success btn-sm' role="button"><a class="text-white text-uppercase" href="bookingaccepted.php?id=<?php echo $id; ?>">Approve</a></button>
                     <button type="button" class='btn btn-danger bg-danger btn-sm' role="button"><a  class="text-white text-uppercase" href="bookingrejected.php?id=<?php echo $id; ?>">Reject</a></button>                
                 </div>	
@@ -224,7 +220,7 @@
     
     
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/js/DashBoard-light-boostrap2.js"></script>
     <script src="../assets/js/DashBoard-light-boostrap.js"></script>
